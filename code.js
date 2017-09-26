100c2cafa84f5e0d97dc9df7c99b6fd658e5e125
Nick Wright <Nick.Wright@advantagesignsupply.com>
2017-09-26 10:10:29 -0400

    Added node, styles, and file edits

--- code.js
+++ code.js
@@ -1,24 +1,3 @@
-$(document).ready(function() {
-  validate();
-  $('input').on('keyup', validate);
+$(function(){
+       $("input").prop('required',true);
 });
-
-function validate() {
-  var inputsWithValues = 0;
-
-  // get all input fields except for type='submit'
-  var myInputs = $("input:not([type='submit'])");
-
-  myInputs.each(function(e) {
-    // if it has a value, increment the counter
-    if ($(this).val()) {
-      inputsWithValues += 1;
-    }
-  });
-
-  if (inputsWithValues == myInputs.length) {
-    $("input[type=submit]").prop("disabled", false);
-  } else {
-    $("input[type=submit]").prop("disabled", true);
-  }
-}
